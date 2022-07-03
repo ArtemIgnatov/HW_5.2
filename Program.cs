109b0f56159ef9d722e5b1059061df6db737530b
Artem Ignatov <ghettoarabdog@gmail.com>
2022-07-03 22:05:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_5._2
+{
+    internal class Program
+    {
+        static string SplitWord(string text)
+        {
+            string[] wordsSplit = text.Split(' ',',','.','!','?');
+            string newText = ReverseWord(wordsSplit);
+            return newText;
+        }
+        static string ReverseWord(string[] text) 
+        {
+            Array.Reverse(text);
+            string str = string.Join(" ", text);
+            return str;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+
+            string newText = Console.ReadLine();
+
+            newText = SplitWord(newText);
+
+
+            Console.WriteLine(newText);
+            Console.ReadLine();
+        }
+    }
+}
